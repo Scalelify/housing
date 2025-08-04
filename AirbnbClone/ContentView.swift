61cf0314ecde561631527175eddbefe5074cd22e
Scalelify <support@scalelify.com>
2025-08-04 20:26:19 +0200

    Manual sync from Scaleify - AirbnbClone/ContentView.swift

---
+++ AirbnbClone/ContentView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            HomeView()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
